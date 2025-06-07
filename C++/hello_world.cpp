c32a3f8b210b3a7dfc686d0361b418a733364b74
Anthony Aleman <jarethaleman0209@gmail.com>
2025-06-07 15:03:53 -0400

    laptop 1

---
+++ C++/hello_world.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct car{
+    int iCost;
+    int mpg;
+    int miles = 15000;
+};
+
+int findCheaper(car arry[]){
+return 0;
+};
+
+int main(int argc, char** argv){
+    car car1;
+    car1.iCost = 25000; car1.mpg = 50;
+    car car2;
+    car2.iCost = 2000; car2.mpg = 30;
+
+    car array[2];
+    array[0] = car1;
+    array[1] = car2;
+
+    return 0;
+}
